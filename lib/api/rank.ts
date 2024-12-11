c67d2d640c3b5ae5e84bdee8233c28a4fdaf6fc8
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-12-11 14:53:16 +0100

    update

--- lib/api/rank.ts
+++ lib/api/rank.ts
@@ -6,6 +6,8 @@
 // Delete rank http://localhost:1337/api/ranks/:id
 // A rank need to pass the time in seconds, the userid
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 export interface Rank {
   id: string;
   time: number;
@@ -13,28 +15,39 @@ export interface Rank {
 }
 
 export const fetchRanks = async () => {
-  const response = await fetch('http://localhost:1337/api/ranks');
-  const data = await response.json();
-  return data;
-};
-
-export const createRank = async (time: number, userId: string) => {
+  const token = await AsyncStorage.getItem('token');
   const response = await fetch('http://localhost:1337/api/ranks', {
-    method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify({ time, userId }),
   });
   const data = await response.json();
   return data;
 };
 
+export const createRank = async (time: number, userId: string) => {
+  const token = await AsyncStorage.getItem('token');
+  const response = await fetch('http://localhost:1337/api/ranks', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+      body: JSON.stringify({ time, userId }),
+    },
+    body: JSON.stringify({ time, userId }),
+  });
+
+  const data = await response.json();
+  return data;
+};
+
 export const updateRank = async (id: string, time: number, userId: string) => {
+  const token = await AsyncStorage.getItem('token');
   const response = await fetch(`http://localhost:1337/api/ranks/${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({ time, userId }),
   });
@@ -43,8 +56,12 @@ export const updateRank = async (id: string, time: number, userId: string) => {
 };
 
 export const deleteRank = async (id: string) => {
+  const token = await AsyncStorage.getItem('token');
   const response = await fetch(`http://localhost:1337/api/ranks/${id}`, {
     method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
   const data = await response.json();
   return data;
